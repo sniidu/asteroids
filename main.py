c09365661111a82c3a4ca0578a9302dfae06ccc4
sniidu <sniidu@users.noreply.github.com>
2024-08-31 13:30:37 +0300

    functioning player class

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import pygame
-import constants
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    # Setting up display
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        # Redraw screen
         pygame.display.flip()
+        # Set waiting time so that game pauses for 1/60 seconds after iteration (60 fps)
+        # Store deltatime (seconds since last time .tick was called)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
