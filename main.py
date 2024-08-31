6b5f0e3e298b7e3ad63cdc8d094532356b37bfe5
sniidu <sniidu@users.noreply.github.com>
2024-08-31 12:34:26 +0300

    pygame init

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+import constants
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
